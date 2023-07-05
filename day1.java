559c750d0c7c5e135aca7018471511b3ce653dd9
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-07-05 22:41:25 +0530

    day1.java

---
+++ day1.java
@@ -0,0 +1,43 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+        ////////////////////////////////////////////////////output/////////////////////////////////////////////////////
+        
+    
+        System.out.println("Hello, World! with java");
+        System.out.print("Hello, World! with java\n");
+        System.out.print("Hello, World! with java\n helloo guys\n bye");
+        //to print in next line we will use println
+        //or we can use \n to go to next line
+
+      
+        /////////////////////////////////////////////print the pattern/////////////////////////////////////////////////////
+     
+
+        System.out.println("*\n**\n***\n****");
+
+   
+        ///////////////////////////////////////////////variables//////////////////////////////////////////////////////////////
+       
+        int a =22;
+        int b=23;
+        int sum=a+b;
+        int mul=a*b;
+        System.out.println(sum);
+        System.out.println(mul);
+        string name="aayush";
+        System.out.println(name);
+       
+        /////////////////////////////////////////////////question 1////////////////////////////////////////////////////////
+      
+        int a=10;
+        int b=15;
+        int c=(a+b)/(a-b);
+        System.out.println(c);
+      
+    }
+}
+
+
+
+
