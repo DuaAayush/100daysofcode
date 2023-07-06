c11af7ca8d3cba875f1a8b6ee97ff855458796e8
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-07-07 01:56:57 +0530

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////conditional statement//////////////////////////////////////////////////
+///////////////////if else elseif////////////////////////////////////////
+import java.util.*;
+public class condstatement {
+    public static void main(String[]args){
+    Scanner sc= new Scanner(System.in);
+    int a= sc.nextInt();
+    if(a>18){
+        System.out.println("adult");
+    }
+    else{
+        System.out.println("kiddoos");
+    }
+    int a= sc.nextInt();
+    int b= sc.nextInt();
+    if(a==b){
+        System.out.println("a=b");
+    }
+    else if(a>b){
+        System.out.println("a>b");
+    }
+    else{
+        System.out.println("b>a");
+    }
+      ///////////////////switch////////////////////////////////
+    int a=sc.nextInt();
+    switch (a){
+        case 1:System.out.println("hello");
+        break;
+        case 2:System.out.println("bonjour");
+        break;
+        case 3:System.out.println("namaste");
+        break;
+        default:System.out.println("bye");
+    }
+}
+}
