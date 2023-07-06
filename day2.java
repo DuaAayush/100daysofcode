e3a312af3b06968af737e29ef5f1f218ad6796d3
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-07-07 01:54:23 +0530

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////input and output/////////////////////////////////////////////////////////////////////////
+import java.util.*;
+public class input {
+    public static void main(String[]args){
+         Scanner sc = new Scanner(System.in);
+         String firstname = sc.nextLine();
+         System.out.println(firstname);
+         String name = sc.nextLine();
+         System.out.println(name);
+         int rollno = sc.nextInt();
+         System.out.println(rollno);
+         Float percent = sc.nextFloat();
+         System.out.println(percent);
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int sum=a+b;
+        System.out.println(sum);
+
+    }
+}
+ import java.util.*;
+public class inpout {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+    } 
+}
