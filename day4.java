239186b424ac2bda1fc997f34290be5b3e79c905
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-07-09 01:59:25 +0530

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class loops {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for (int count=0; count<a-1;count=count+1){
+            System.out.println("helloworld");
+          }
+        }
+        for (int c=0;c<11;c=c+1){
+            System.out.println(c);
+
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int f=0;
+       for (int i=0;i<=n;i++){
+        f=f+i;
+        
+       }
+       System.out.println(f);
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int f;
+    for(int i=0;i<=10;i++){
+        f=n*i;
+        System.out.println(f);
+    }
+    }
+    
+}
