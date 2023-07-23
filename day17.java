e6719a5f70be5c276af72d75de150e24e32b2b19
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-07-24 00:16:33 +0530

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,35 @@
+public class FactorialExample {
+    public static void main(String[] args) {
+        int number = 5;
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
+
+public class FibonacciRecursive {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("Fibonacci sequence up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
