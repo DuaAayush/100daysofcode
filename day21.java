d27a3e98101a8a4714c8d74847248cc9296fb248
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-08-26 12:19:11 +0530

    Update day21.java

--- day21.java
+++ day21.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class RecursionPractice {
     public static void main(String[] args) {
-        // 1. Fibonacci Sequence
+        // 1 Fibonacci Sequence
         int n = 10;
         System.out.println("Fibonacci sequence up to " + n + " terms:");
         for (int i = 0; i < n; i++) {
@@ -10,50 +10,50 @@ public class RecursionPractice {
         }
         System.out.println();
 
-        // 2. Factorial
+        // 2 Factorial
         int number = 5;
         System.out.println("Factorial of " + number + " is: " + factorial(number));
 
-        // 3. Sum of Array Elements
+        // 3 Sum of Array Elements
         int[] arr = {2, 5, 8, 1, 3};
         System.out.println("Sum of array elements: " + sumOfArray(arr));
 
-        // 4. Exponentiation
+        // 4 Exponentiation
         int x = 2;
         int exponent = 5;
         System.out.println(x + " raised to the power of " + exponent + " is: " + power(x, exponent));
 
-        // 5. Palindrome Check
+        // 5 Palindrome Check
         String str = "radar";
         System.out.println("Is \"" + str + "\" a palindrome? " + isPalindrome(str));
 
-        // 6. GCD (Greatest Common Divisor)
+        // 6 GCD (Greatest Common Divisor)
         int a = 48;
         int b = 18;
         System.out.println("GCD of " + a + " and " + b + " is: " + gcd(a, b));
 
-        // 7. Subset Sum
+        // 7 Subset Sum
         int[] set = {3, 34, 4, 12, 5, 2};
         int targetSum = 9;
         System.out.println("Does a subset with sum " + targetSum + " exist? " + hasSubsetSum(set, targetSum));
 
-        // 8. Tower of Hanoi
+        // 8 Tower of Hanoi
         int numOfDisks = 3;
         System.out.println("\nSteps to solve Towers of Hanoi with " + numOfDisks + " disks:");
         solveTowersOfHanoi(numOfDisks, 'A', 'C', 'B');
 
-        // 9. Generate Permutations
+        // 9 Generate Permutations
         String inputStr = "abc";
         System.out.println("\nPermutations of \"" + inputStr + "\":");
         generatePermutations(inputStr, "");
 
-        // 10. Binary Search
+        // 10 Binary Search
         int[] sortedArr = {1, 3, 5, 7, 9, 11};
         int targetElement = 7;
         System.out.println("Index of " + targetElement + " in the array: " + binarySearch(sortedArr, targetElement));
     }
 
-    // 1. Fibonacci Sequence
+    // 1 Fibonacci Sequence
     public static int fibonacci(int n) {
         if (n == 0) {
             return 0;
