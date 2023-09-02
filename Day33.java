24af377dece824f93d62c9422d2ef833255fdf86
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-09-03 00:38:28 +0530

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,36 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTreeDemo {
+    // In-order traversal of a binary tree
+    public static void inOrderTraversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrderTraversal(root.left);
+        System.out.print(root.val + " ");
+        inOrderTraversal(root.right);
+    }
+
+    public static void main(String[] args) {
+        // Creating a simple binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.print("In-order traversal: ");
+        inOrderTraversal(root); // Output: 4 2 5 1 3
+    }
+}
