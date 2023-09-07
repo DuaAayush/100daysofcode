20ce9dd5910dc81e4c6376dbe1f46993cb1a9966
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-09-07 19:40:16 +0530

    Create day38.java

---
+++ day38.java
@@ -0,0 +1,31 @@
+LeetCode Problem 1: Valid Parentheses 
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses solution = new ValidParentheses();
+        String input = "{[()]}";
+        boolean isValid = solution.isValid(input);
+        System.out.println("Is valid: " + isValid); // Output: true
+    }
+}
