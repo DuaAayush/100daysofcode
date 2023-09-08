51b2c7e72d54c8feeba5128af3552ed8e24c59c4
DuaAayush <122432066+DuaAayush@users.noreply.github.com>
2023-09-08 17:24:19 +0530

    Create day39.java

---
+++ day39.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(solutions, board, 0, n);
+        return solutions;
+    }
+
+    private void backtrack(List<List<String>> solutions, char[][] board, int row, int n) {
+        if (row == n) {
+            solutions.add(constructSolution(board));
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(solutions, board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> constructSolution(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            solution.add(new String(board[i]));
+        }
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens();
+        int n = 4; // Change n to the desired board size
+        List<List<String>> solutions = nQueens.solveNQueens(n);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
